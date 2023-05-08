aaf1f4467fa0671e90702beb1ff7f9ee184a2a7f
Kalid Geleta <kalidgeleta28@gmail.com>
2023-05-08 15:11:27 +0300

    cooling

--- scrt.js
+++ scrt.js
@@ -1,14 +1,83 @@
 
+imgcontainer =document.querySelector(".imgcontainer");
+imglist=document.querySelector(".imglist")
+rightbutton=document.querySelector(".right_btn")
+leftbutton=document.querySelector(".left_btn")
+arrayimglist=Array.from(document.querySelector(".imglist").children);
+imgitem=document.querySelector("imgitem");
+navdots=document.querySelector(".navdots")
+arraynavdots=Array.from(document.querySelector(".navdots").children)
 
-document.querySelector(".humburger").addEventListener("click",()=>{
-    console.log("no");
-    document.querySelector(".navigation").classList.toggle("clicked");
-    
-});
-window.addEventListener("scroll",()=>{
-    x=window.scrollY;
-    y=document.documentElement.scrollHeight-window.innerHeight;
-    document.querySelector(".head").classList.add("scrolled");
-   
+imagewidth=arrayimglist[0].getBoundingClientRect().width;
+
+arrayimglist.forEach((items,index)=>{
+    items.style.left=index*imagewidth+"px";
+    console.log(getComputedStyle(items).left);
 })
-document.querySelector(".head").classList.remove("scrolled");
+
+rightbutton.addEventListener("click",()=>{
+   
+    currentimg=document.querySelector(".imgitem.current-img");
+    currentdot=document.querySelector(".dot.current-img");
+    nextimg=currentimg.nextElementSibling;
+    nextdot=currentdot.nextElementSibling;
+  
+   
+    imglist.style.transform="translateX(-"+getComputedStyle(nextimg).left
+   
+    currentimg.classList.remove("current-img");
+    nextimg.classList.add("current-img");
+
+
+    currentdot.classList.remove("current-img");
+    nextdot.classList.add("current-img");
+  
+    
+
+});
+
+leftbutton.addEventListener("click",()=>{
+   
+    currentimg=document.querySelector(".imgitem.current-img");
+    currentdot=document.querySelector(".dot.current-img");
+    perviousimg=currentimg.previousElementSibling;
+    prevdot=currentdot.previousElementSibling;
+   
+    imglist.style.transform="translateX(-"+getComputedStyle(perviousimg).left
+   
+    currentimg.classList.remove("current-img");
+    perviousimg.classList.add("current-img");
+
+
+    currentdot.classList.remove("current-img");
+    prevdot.classList.add("current-img");
+  
+    
+
+});
+
+
+navdots.addEventListener("click",(e)=>{
+    currentimg=document.querySelector(".imgitem.current-img");
+    currentdot=document.querySelector(".dot.current-img");
+    tar=e.target;
+   ix= arraynavdots.findIndex((dot)=>{
+  if(tar===dot){
+    return true;
+  }
+    });
+//    arraynavdots.forEach((f)=>{console.log(f)});
+   console.log(ix);
+    imglist.style.transform="translateX(-"+getComputedStyle(arrayimglist[ix]).left
+    currentimg.classList.remove("current-img");
+    arrayimglist[ix].classList.add("current-img");
+    currentdot.classList.remove("current-img");
+    arraynavdots[ix].classList.add("current-img");
+    
+   console.log(e.target)
+
+});
+
+console.log(navdots);
+
+
